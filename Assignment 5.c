47a687d69b683e02392533bf336ef009d15cc923
AyushKumarSrivas <117811777+AyushKumarSrivas@users.noreply.github.com>
2024-07-09 20:26:18 +0530

    Add files via upload

---
+++ Assignment 5.c
@@ -0,0 +1,131 @@
+/*1.Write a program to print MySirG N times on the screen.
+main()
+{
+    int n,i;
+    printf("Enter the number of times you want to print:");
+    scanf("%d",&n);
+    for(i=1;i<=n;i++)
+    {
+
+        printf("MySirG\n");
+    }
+}
+*/
+
+/*2.Write a program to print the first N natural numbers.
+main()
+{
+    int n,i;
+    printf("Enter the number:");
+    scanf("%d",&n);
+    for(i=1;i<=n;i++)
+    {
+        printf("%d\n",i);
+    }
+}
+*/
+
+/*3.Write a program to print the first N natural numbers in reverse order.
+main()
+{
+    int n,i;
+    printf("Enter the number:");
+    scanf("%d",&n);
+    for(i=n;i>=1;i--)
+    {
+        printf("%d\n",i);
+    }
+}
+*/
+
+/*4.Write a program to print the first N odd natural numbers.
+main()
+{
+    int n,i;
+    printf("Enter the number:");
+    scanf("%d",&n);
+    for(i=1;i<=n;i++)
+    {
+        printf("%d\n",2*i-1);
+    }
+}
+*/
+
+/*5.Write a program to print the first N odd natural numbers in reverse order.
+main()
+{
+    int n,i;
+    printf("Enter the number:");
+    scanf("%d",&n);
+    for(i=n;i>=1;i--)
+    {
+        printf("%d\n",2*i-1);
+    }
+}
+*/
+
+/*6.Write a program to print the first N even natural numbers.
+main()
+{
+    int n,i;
+    printf("Enter the number:");
+    scanf("%d",&n);
+    for(i=1;i<=n;i++)
+    {
+        printf("%d\n",2*i);
+    }
+}
+*/
+
+/*7.Write a program to print the first N even natural numbers in reverse order.
+main()
+{
+    int n,i;
+    printf("Enter the number:");
+    scanf("%d",&n);
+    for(i=n;i>=1;i--)
+    {
+        printf("%d\n",2*i);
+    }
+}
+*/
+
+/*8.Write a program to print the squares of the first N natural numbers.
+main()
+{
+    int n,i;
+    printf("Enter the number:");
+    scanf("%d",&n);
+    for(i=1;i<=n;i++)
+    {
+        printf("%d\n",i*i);
+    }
+}
+*/
+
+/*9.Write a program to print the cubes of the first N natural numbers.
+main()
+{
+    int n,i;
+    printf("Enter the number:");
+    scanf("%d",&n);
+    for(i=1;i<=n;i++)
+    {
+        printf("%d\n",i*i*i);
+    }
+}
+*/
+
+//10.Write a program to print the table of N.
+main()
+{
+    int n,i;
+    printf("Enter the number:");
+    scanf("%d",&n);
+    for(i=1;i<=10;i++)
+    {
+        printf("%d * %d = %d\n",n,i,n*i);
+    }
+}
+
+
